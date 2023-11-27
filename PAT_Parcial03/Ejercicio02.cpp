069d705e6437ae4b46bd7680f9287aa48a13abc1
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-11-26 20:13:13 -0600

    Ejercicio 2

--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -1,6 +1,38 @@
 #include "Ejercicio02.h"
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
 
 vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
 {
-    return nullptr;
+	unordered_map<string, int> mapadeADN;
+	vector<string>* result = new vector<string>;
+	int max = 10;
+
+	Node<char>* current = head;
+
+	while (current)
+	{
+		string ADN;
+		Node<char>* temp = current;
+		for (int i = 0; i < max && temp; i++, temp = temp->next)
+		{
+			ADN += temp->value;
+		}
+
+		if (ADN.length() == max) {
+			mapadeADN[ADN]++;
+			if (mapadeADN[ADN] > 1) {
+				auto it = find(result->begin(), result->end(), ADN);
+				if (it == result->end())
+					result->push_back(ADN);
+			}
+		}
+		else {
+			break;
+		}
+		current = current->next;
+	}
+
+	return result;
 }
