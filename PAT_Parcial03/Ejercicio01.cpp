607099658bba7b2e20144f919a778bbe5947dc9d
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-11-26 18:47:24 -0600

    Ejercicio 1

--- PAT_Parcial03/Ejercicio01.cpp
+++ PAT_Parcial03/Ejercicio01.cpp
@@ -1,7 +1,54 @@
 #include "Ejercicio01.h"
+#include <queue>
+using namespace std;
+
+struct Comparador {
+    bool operator()(Node<int>*& a, Node<int>*& b) {
+        return a->value < b->value;
+    }
+};
 
 Node<int>* Ejercicio01::mergeLists(Node<Node<int>*>* lists)
 {
+    priority_queue<Node<int>*, vector<Node<int>*>, Comparador> colaprior;
 
-    return nullptr;
+    while (lists)
+    {
+        if (lists->value)
+        {
+            colaprior.push(lists->value);
+        }
+        lists = lists->next;
+    }
+
+    Node<int>* head = nullptr;
+    Node<int>* tail = nullptr;
+
+    while (!colaprior.empty())
+    {
+        Node<int>* current = colaprior.top();
+        colaprior.pop();
+
+        if (!head)
+        {
+            head = tail = current;
+        }
+        else
+        {
+            tail->next = current;
+            tail = current;
+        }
+
+        if (current->next)
+        {
+            colaprior.push(current->next);
+        }
+    }
+
+    if (tail)
+    {
+        tail->next = nullptr;
+    }
+
+    return head;
 }
